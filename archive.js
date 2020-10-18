4bf6662f0e56b7d09e12611441e160ec0fd3dc6d
mosmolov <57736073+mosmolov@users.noreply.github.com>
2020-10-18 18:10:18 -0400

    Changed up archive.js
    -Changing success picture to Borat gif
    -Took out personal details
    -Added Admin permissions check

--- archive.js
+++ archive.js
@@ -8,19 +8,18 @@ module.exports = {
     description: 'Archive message to separate category after asking user for input',
     execute(message,args){
         
-            
-            const newHome = args[0];
+            if(message.member.hasPermission('ADMINISTRATOR')){
+            const newHome = args[0]; //Category the channel is moving to
             console.log(message.channel);
-            message.channel.setParent(newHome);
+            message.channel.setParent(newHome); //Move the channel to new category
             
             console.log(`New parent of ${message.channel.name}: ${newHome}`)
-            
-            const hook = new Discord.WebhookClient('webhook id','webhook token');
+            const hook = new Discord.WebhookClient('webhook id','webhook token'); //Optional Success Logger -- Just fill in the webhook details
             const embed = new Discord.MessageEmbed({
                 title: 'Successfully Archived!',
-                description: `New parent of #${message.channel.name}: <#${newHome}>`,
+                description: `New parent of <#${message.channel.id}>: <#${newHome.name}>`,
                 image: {
-                    url: 'https://i.pinimg.com/originals/7b/a4/ef/7ba4ef400a13a91e7967a3f6e5c6b528.jpg'
+                    url: 'https://media.giphy.com/media/a0h7sAqON67nO/giphy.gif'
                 },
                 timestamp: message.createdAt,
             });
@@ -28,7 +27,10 @@ module.exports = {
                 
            
             
-            
+        }
+        else{
+            message.channel.send('Nice try :smirk:');
+        }
         
     }   
 }
