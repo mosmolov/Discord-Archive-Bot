ae09abbf3f89e5205c67abd1de287565238bccad
mosmolov <57736073+mosmolov@users.noreply.github.com>
2020-10-18 23:08:39 -0400

    General Updates
    -Removed unnecessary packages
    -Improved error handling
    -improved cleanliness

--- autoarchive.js
+++ autoarchive.js
@@ -1,21 +1,35 @@
-const { CronJob } = require('cron');
 const Discord = require('discord.js');
 const client = new Discord.Client();
-const cron = require('cron.js');
+
 module.exports = {
   name: 'autoarchive',
   description: 'Automatically archives channels based on release date',
   execute(message, args) {
-    if(message.member.hasPermission('ADMINISTRATOR')){ //Check user permissions to make sure user is Administrator
+    if(!message.member.hasPermission('ADMINISTRATOR')){ //Check user permissions to make sure user is Administrator
+      message.channel.send('Nice try :smirk:');
+    }
+    else{
+      try{
     const oldHome = args[0]; // oldHome is the old category
     const newHome = args[1]; //newHome is the category you're archiving the channels to
+      }
+      catch(err){
+        message.send('Command Failed. Check arguments and try again!');
+      }
+    
+    
     var date = new Date();
     var day = date.getDate();
-    console.log(`Todays date is ${day}`);
-    const guild = message.guild;
-    const categoryChannels = guild.channels.cache.filter(channel => channel.type === "text" && channel.parentID === oldHome); //filter all of the channels for only the Release Category (oldHome)
-    categoryChannels.forEach(channel =>{ 
-        let release_channel = channel.name.split('-');
+    // console.log(`Todays date is ${day}`);
+    try{
+      const guild = message.guild;
+
+      const categoryChannels = guild.channels.cache.filter(channel => channel.type === "text" && channel.parentID === oldHome); //filter all of the channels for only the Release Category (oldHome)
+    
+    
+  
+        categoryChannels.forEach(channel =>{ 
+        let release_channel = channel.name.split('-'); //Split up channel name 
         
         let month = release_channel[0]; // first element in array release_channel is the month
         let release_day = release_channel[1]; //second element is the day
@@ -25,12 +39,18 @@ module.exports = {
           console.log(`Now Archiving ${channel.name}`);
           channel.setParent(newHome); //Moves channel to new category
         }
-       
-        
-    });      
-  }    
-  else{
-    message.channel.send('Nice try :smirk:');
+      
+   
+    });
   }
+  catch(err){
+    message.channel.send('Command Failed. Check Arguments and Try Again!');
+  }
+  }
+  
+         
+   
+    
+
 }
 }
