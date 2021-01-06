22584e37d48c6ca39ca1f1081529560606f8daa4
mosmolov <57736073+mosmolov@users.noreply.github.com>
2021-01-06 10:20:59 -0500

    update index.js

--- index.js
+++ index.js
@@ -1,36 +1,26 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
 const prefix = '*';
-const fs = require('fs');
-client.commands = new Discord.Collection();
-
-
-const commandFiles = fs.readdirSync('./commands').filter(file=> file.endsWith('.js'))
-for(const file of commandFiles){
-    const command = require(`./commands/${file}`);
-    client.commands.set(command.name, command);
-}
+const bottoken = require('./config.json');
 client.once('ready', () => {
     console.log('I am Online!');
 })
 
-client.on('message',message=>{
-    if(!message.content.startsWith(prefix)|| message.author.bot) return;
-    const args = message.content.slice(prefix.length).split(/ +/);
+client.on('message', message => {
+    if(!message.content.startsWith(prefix)) return;
+    if(!message.member.hasPermission('ADMINISTRATOR')) message.channel.send('Must be Administrator!');
+    var args = message.content.slice(prefix.length).split(/ +/);
     const command = args.shift().toLowerCase();
-
-    if(command==='archive'){
-        client.commands.get('archive').execute(message, args);
+    
+    if(command === 'autoarchive'){
+        const autoarchive = require('./commands/autoarchive');
+        autoarchive(message,args);
     }
-    if(command==='autoarchive'){
-        client.commands.get('autoarchive').execute(message, args);
-    }
-    if(command==='categorychannels'){
-        client.commands.get('categorychannels').execute(message, args);
-    }
-    if(command==='help'){
-        client.commands.get('help').execute(message, args);
+    if(command === 'help'){
+        const help = require('./commands/help');
+        help();
     }
 });
 
-client.login('insert token here');
+
+client.login(bottoken);
