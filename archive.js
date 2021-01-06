a5ebfc5fddfc2a14b7309860fa3406105e463f0b
mosmolov <57736073+mosmolov@users.noreply.github.com>
2021-01-06 11:04:30 -0500

    Delete archive.js

--- archive.js
+++
@@ -1,7 +0,0 @@
-function archive(message,args){
-    const newHome = args[0];
-    message.channel.setParent(newHome);
-    message.channel.send(`Successfully Archived ${message.channel}`);
-}
-
-module.exports = { archive }
